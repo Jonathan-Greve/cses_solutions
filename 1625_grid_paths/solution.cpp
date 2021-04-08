19e8bae43ed67f8474a31027bb05a1dccc52aaab
Jonathan-Greve <jonagrev@gmail.com>
2021-04-08 15:13:04 +0800

    Working fast

--- 1625_grid_paths/solution.cpp
+++ 1625_grid_paths/solution.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 static const int board_size = 7;
+string moves;
 
-int solve(string moves, bool curr_path[board_size * board_size - 1], int move_index, int x, int y);
+int solve(bool curr_path[board_size * board_size - 1], int move_index, int x, int y);
 
 bool can_go_up(int x, int y, bool curr_path[board_size * board_size - 1])
 {
@@ -42,7 +43,7 @@ bool can_go_left(int x, int y, bool curr_path[board_size * board_size - 1])
     return false;
 }
 
-int try_move(string moves, bool curr_path[board_size * board_size - 1], int move_index, int x, int y, char move)
+int try_move(bool curr_path[board_size * board_size - 1], int move_index, int x, int y, char move)
 {
     int num_solutions = 0;
     switch (move)
@@ -51,7 +52,7 @@ int try_move(string moves, bool curr_path[board_size * board_size - 1], int move
         if (can_go_up(x, y, curr_path) && !(!(can_go_left(x, y, curr_path) || can_go_right(x, y, curr_path)) && can_go_down(x, y, curr_path)))
         {
             curr_path[(y - 1) * board_size + x] = true;
-            num_solutions = solve(moves, curr_path, move_index + 1, x, y - 1);
+            num_solutions = solve(curr_path, move_index + 1, x, y - 1);
             curr_path[(y - 1) * board_size + x] = false;
             return num_solutions;
         }
@@ -60,7 +61,7 @@ int try_move(string moves, bool curr_path[board_size * board_size - 1], int move
         if (can_go_down(x, y, curr_path) && !(!(can_go_left(x, y, curr_path) || can_go_right(x, y, curr_path)) && can_go_up(x, y, curr_path)))
         {
             curr_path[(y + 1) * board_size + x] = true;
-            num_solutions = solve(moves, curr_path, move_index + 1, x, y + 1);
+            num_solutions = solve(curr_path, move_index + 1, x, y + 1);
             curr_path[(y + 1) * board_size + x] = false;
             return num_solutions;
         }
@@ -69,7 +70,7 @@ int try_move(string moves, bool curr_path[board_size * board_size - 1], int move
         if (can_go_right(x, y, curr_path) && !(!(can_go_up(x, y, curr_path) || can_go_down(x, y, curr_path)) && can_go_left(x, y, curr_path)))
         {
             curr_path[y * board_size + x + 1] = true;
-            num_solutions = solve(moves, curr_path, move_index + 1, x + 1, y);
+            num_solutions = solve(curr_path, move_index + 1, x + 1, y);
             curr_path[y * board_size + x + 1] = false;
             return num_solutions;
         }
@@ -78,7 +79,7 @@ int try_move(string moves, bool curr_path[board_size * board_size - 1], int move
         if (can_go_left(x, y, curr_path) && !(!(can_go_up(x, y, curr_path) || can_go_down(x, y, curr_path)) && can_go_right(x, y, curr_path)))
         {
             curr_path[y * board_size + x - 1] = true;
-            num_solutions = solve(moves, curr_path, move_index + 1, x - 1, y);
+            num_solutions = solve(curr_path, move_index + 1, x - 1, y);
             curr_path[y * board_size + x - 1] = false;
             return num_solutions;
         }
@@ -87,42 +88,40 @@ int try_move(string moves, bool curr_path[board_size * board_size - 1], int move
     return num_solutions;
 }
 
-int solve(string moves, bool curr_path[board_size * board_size - 1], int move_index, int x, int y)
+int solve(bool curr_path[board_size * board_size - 1], int move_index, int x, int y)
 {
     int num_solutions = 0;
-    static char UDLR[4] = {'U', 'D', 'L', 'R'};
-    if (x == 0 && y == (board_size - 1) && move_index == (board_size * board_size - 1))
+    if (x == 0 && y == (board_size - 1))
     {
-        num_solutions = 1;
+        if (move_index == (board_size * board_size - 1))
+            num_solutions = 1;
     }
     else
     {
         if (moves[move_index] == '?')
         {
-            for (auto &&move : UDLR)
-            {
-                num_solutions += try_move(moves, curr_path, move_index, x, y, move);
-            }
-       
+            num_solutions += try_move(curr_path, move_index, x, y, 'U');
+            num_solutions += try_move(curr_path, move_index, x, y, 'D');
+            num_solutions += try_move(curr_path, move_index, x, y, 'L');
+            num_solutions += try_move(curr_path, move_index, x, y, 'R');
+        }
+
         else
         {
-            num_solutions += try_move(moves, curr_path, move_index, x, y, moves[move_index]);
+            num_solutions += try_move(curr_path, move_index, x, y, moves[move_index]);
         }
     }
-
     return num_solutions;
 }
 
 int main()
 {
-    string moves;
-
     getline(cin, moves);
 
     bool curr_path[board_size * board_size - 1];
     curr_path[0] = true;
 
-    int num_solutions = solve(moves, curr_path, 0, 0, 0);
+    int num_solutions = solve(curr_path, 0, 0, 0);
 
     cout << num_solutions << "\n";
 
